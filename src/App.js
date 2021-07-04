b2d22bfe1214da5c825b91fd58077f06e3b4a770
Dylan Gatlin <dgatlin5@gmail.com>
2021-07-04 13:37:29 -0600

    Init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
 
+
+export class TextBox extends Component {
+    constructor(name) {
+        super(undefined);
+        this.name = name
+    }
+    displayName = TextBox.name
+    render() {
+        return (
+                <form>
+                    <label>{this.name}:
+                    <input type="text" name={this.name}/>
+                    </label>
+                    <input type="submit" value="Submit"/>
+                </form>
+        )
+    }
+}
+
+function render_boxes() {
+    let box_names = ["Time", "Reps", "Resistance", "Difficulty"]
+    let boxes = []
+    for(let name in box_names) {
+        const box = new TextBox(name)
+        boxes.push(box)
+    }
+    let x = null
+    for(let box in boxes) {
+        console.log(box.render())
+        x = x + box.render()
+    }
+    return(x)
+}
 function App() {
+    const box = new TextBox("Time")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Workout Logging">
+        {box.render()}
+        <script src={render_boxes}/>
+      <button>
+        Submit
+      </button>
     </div>
   );
 }
